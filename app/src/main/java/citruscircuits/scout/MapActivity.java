b00a68e07149770460c8213c14bbda705fecccc2
Calvin <cwhuang2002@icloud.com>
2018-06-17 10:39:10 -0700

    Fixed Gradle

--- app/src/main/java/citruscircuits/scout/MapActivity.java
+++
@@ -1,21 +0,0 @@
-package citruscircuits.scout;
-
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-
-/**
- * Created by sam on 6/11/18.
- */
-public class MapActivity extends AppCompatActivity{ 
-    public void onCreate(Bundle savedInstanceState){ 
-        super.onCreate(savedInstanceState); 
-        setContentView(R.layout.map_activity); 
-    }  
-
-}
