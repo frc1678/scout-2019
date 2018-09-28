50b967860aca97dd41bd00e8d6c7d8dea295a08f
EmilyTLin <powerless300@gmail.com>
2018-09-27 22:18:26 -0700

    Coordinates, intake and placement of shapes

--- app/src/main/java/citruscircuits/scout/A1A.java
+++ app/src/main/java/citruscircuits/scout/A1A.java
@@ -1,14 +1,20 @@
 package citruscircuits.scout;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
@@ -27,6 +33,8 @@ public class A1A extends DialogMaker implements View.OnClickListener{
 
     public boolean incapChecked = false;
     public boolean startTimer = true;
+    public boolean shapeCheck = false;
+    public boolean noShape = false;
 
     public TextView tv_team;
 
@@ -53,6 +61,11 @@ public class A1A extends DialogMaker implements View.OnClickListener{
     public RadioButton rb_red_center;
     public RadioButton rb_red_left;
 
+    public RelativeLayout overallLayout;
+
+    public ImageView iv;
+    public ImageView iv2;
+
     public Handler handler = new Handler();
     public Runnable runnable = new Runnable() {
         public void run() {
@@ -67,12 +80,15 @@ public class A1A extends DialogMaker implements View.OnClickListener{
         if(AppCc.getSp("mapOrientation",99) != 99){
             if(AppCc.getSp("mapOrientation", 99) == 0){
                 setContentView(R.layout.activity_map_rb);
+                overallLayout = findViewById(R.id.fieldrb);
             }else {
                 setContentView(R.layout.activity_map_br);
+                overallLayout = findViewById(R.id.fieldbr);
             }
         }else{
             AppCc.setSp("mapOrientation", 0);
             setContentView(R.layout.activity_map_rb);
+            overallLayout = findViewById(R.id.fieldrb);
         }
 
         Fragment fragment = new AutoDialog();
@@ -122,10 +138,15 @@ public class A1A extends DialogMaker implements View.OnClickListener{
         rb_red_center = findViewById(R.id.red_starting_position_center);
         rb_red_left = findViewById(R.id.red_starting_position_left);
 
+        iv = new ImageView(getApplicationContext());
+        iv2 = new ImageView(getApplicationContext());
+
         TimerUtil.mTimerView = findViewById(R.id.tv_timer);
         TimerUtil.mActivityView = findViewById(R.id.tv_activity);
 
         tv_team.setText(valueOf(InputManager.mTeamNum));
+
+        addTouchListener();
     }
 
     @Override
@@ -231,5 +252,130 @@ public class A1A extends DialogMaker implements View.OnClickListener{
             InputManager.mStartingPosition = "left";
         }
         open(A2A.class, null, false, true);
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private void addTouchListener(){
+        overallLayout.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+//                float x= (int) motionEvent.getX();
+//                float y= (int) motionEvent.getY();
+//                String message = String.format("Coordinates:(%.2f,%.2f)",x,y);
+//                Log.d("hello", message);
+                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                    int x = (int) motionEvent.getX();
+                    int y = (int) motionEvent.getY();
+                    if (x<=1700 && y<=930){
+                        if (!shapeCheck){
+                            overallLayout.removeView(iv2);
+                            RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(
+                                    50,
+                                    50);
+                            //ImageView iv= new ImageView(getApplicationContext());
+                            lp.setMargins(x-25, y-40, 0, 0);
+                            iv.setLayoutParams(lp);
+                            iv.setImageDrawable(getResources().getDrawable(
+                                    R.drawable.triangle_image));
+                            ((ViewGroup) view).addView(iv);
+                            shapeCheck=true;
+                            // overallLayout.removeView(iv);
+                            if (x<=425 && y<=930 && InputManager.mScoutId <=6 || x>75 && x<=285 && y<=575 && InputManager.mScoutId >6){
+                                Log.d("locationInput","1");
+                            }
+                            else if (x>425 && x<=850 && y<=930 && InputManager.mScoutId <=6 || x>285 && x<=575 && y<=575 && InputManager.mScoutId >6){
+                                Log.d("locationInput","2");
+                            }
+                            else if (x>850 && x<=1275 && y<=930 && InputManager.mScoutId <=6 || x>575 && x<=865 && y<=575 && InputManager.mScoutId >6){
+                                Log.d("locationInput","3");
+                            }
+                            else if (x<=1700 && y<=930 && InputManager.mScoutId <=6 || x<=1075 && y<=575 && InputManager.mScoutId >6){
+                                Log.d("locationInput","4");
+                            }
+                        }
+                        else if (shapeCheck){
+                            overallLayout.removeView(iv);
+                            RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(
+                                    50,
+                                    50);
+                            shapeCheck=false;
+                            noShape=true;
+                            if (x>=325 && x<=490 && y>=140 && y<=330 && InputManager.mScoutId <=6 || x>=230 && x<=310 && y>=90 && y<=210 && InputManager.mScoutId >6){
+                                Log.d("locationOutput","Top Left switch");
+                                //ImageView iv = new ImageView(getApplicationContext());
+                                lp.setMargins(x-25, y-40, 0, 0);
+                                iv2.setLayoutParams(lp);
+                                iv2.setImageDrawable(getResources().getDrawable(
+                                        R.drawable.blackcircle));
+                                ((ViewGroup) view).addView(iv2);
+                            }
+                            else if (x>=325 && x<=595 && y>=140 && y<=800 && InputManager.mScoutId <=6 || x>=230 && x<=310 && y>=400 && y<=530 && InputManager.mScoutId >6){
+                                Log.d("locationOutput","Bottom Left switch");
+                                //ImageView iv = new ImageView(getApplicationContext());
+                                lp.setMargins(x-25, y-40, 0, 0);
+                                iv2.setLayoutParams(lp);
+                                iv2.setImageDrawable(getResources().getDrawable(
+                                        R.drawable.blackcircle));
+                                ((ViewGroup) view).addView(iv2);
+                            }
+                            else if (x>=1250 && x<=1400 && y>=140 && y<=330 && InputManager.mScoutId <=6 || x>=780 && x<=900 && y>=100 && y<=210 && InputManager.mScoutId >6){
+                                Log.d("locationOutput","Top Right switch");
+                                //ImageView iv = new ImageView(getApplicationContext());
+                                lp.setMargins(x-25, y-40, 0, 0);
+                                iv2.setLayoutParams(lp);
+                                iv2.setImageDrawable(getResources().getDrawable(
+                                        R.drawable.blackcircle));
+                                ((ViewGroup) view).addView(iv2);
+                            }
+                            else if (x>=1250 && x<=1400 && y>=600 && y<=800 && InputManager.mScoutId <=6 || x>=780 && x<=900 && y>=400 && y<=530 && InputManager.mScoutId >6){
+                                Log.d("locationOutput","Bottom Right switch");
+                                //ImageView iv = new ImageView(getApplicationContext());
+                                lp.setMargins(x-25, y-40, 0, 0);
+                                iv2.setLayoutParams(lp);
+                                iv2.setImageDrawable(getResources().getDrawable(
+                                        R.drawable.blackcircle));
+                                ((ViewGroup) view).addView(iv2);
+                            }
+                            else if (x>=800 && x<=940 && y>=60 && y<=240 && InputManager.mScoutId <=6 || x>=530 && x<=620 && y>=60 && y<=165 && InputManager.mScoutId >6){
+                                Log.d("locationOutput","Scale Top");
+                                //ImageView iv = new ImageView(getApplicationContext());
+                                lp.setMargins(x-25, y-40, 0, 0);
+                                iv2.setLayoutParams(lp);
+                                iv2.setImageDrawable(getResources().getDrawable(
+                                        R.drawable.blackcircle));
+                                ((ViewGroup) view).addView(iv2);
+                            }
+                            else if (x>=790 && x<=940 && y>=700 && y<=870 && InputManager.mScoutId <=6 || x>=530 && x<=620 && y>=450 && y<=560 && InputManager.mScoutId >6){
+                                Log.d("locationOutput","Scale Bottom");
+                                //ImageView iv = new ImageView(getApplicationContext());
+                                lp.setMargins(x-25, y-40, 0, 0);
+                                iv2.setLayoutParams(lp);
+                                iv2.setImageDrawable(getResources().getDrawable(
+                                        R.drawable.blackcircle));
+                                ((ViewGroup) view).addView(iv2);
+                            }
+                            else if (x>=0 && x<=170 && y>=290 && y<=420 && InputManager.mScoutId <=6 || x>=1530 && x<=1700 && y>=490 && y<=630 && InputManager.mScoutId <=6 || x>=0 && x<=120 && y>=210 && y<=290 && InputManager.mScoutId>6 || x>=1000 && x<=1110 && y>=330 && y<=410 && InputManager.mScoutId>6){
+                                Log.d("locationOutput","Exchange");
+                                //ImageView iv = new ImageView(getApplicationContext());
+                                lp.setMargins(x-25, y-40, 0, 0);
+                                iv2.setLayoutParams(lp);
+                                iv2.setImageDrawable(getResources().getDrawable(
+                                        R.drawable.blackcircle));
+                                ((ViewGroup) view).addView(iv2);
+                            }
+//                            else if (x>=790 && x<=940 && y>=700 && y<=870) {
+//                                Log.d("locationOutput", "Left Portal");
+//                            }
+//                            else if (x>=790 && x<=940 && y>=700 && y<=870) {
+//                                Log.d("locationOutput", "Right Portal");
+//                            }
+                        }
+//
+                    }
+                    //purpose: to locate which quadrant
+                }
+                return false;
+            }
+        });
     }
 }
