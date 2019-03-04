02b0ba9741936f60c3e4a186695484f8c36622c7
Emily Lin <32380328+EmilyTLin@users.noreply.github.com>
2019-03-03 16:41:37 -0800

    Update version number from 1.0 to 1.1 (#68)
    * Update version number from 1.1 to 1.2.
    
    Update version number for CVR.
    
    * Change version number from 1.2 to 1.1.

--- app/src/main/java/citruscircuits/scout/Managers/InputManager.java
+++ app/src/main/java/citruscircuits/scout/Managers/InputManager.java
@@ -65,7 +65,7 @@ public class InputManager {
 
     public static Integer mTimerStarted = 0;
     public static boolean mCrossedHabLine = false;
-    public static String mAppVersion = "1.0";
+    public static String mAppVersion = "1.1";
     public static String mAssignmentMode = "";
     public static Integer mAssignmentFileTimestamp = 0;
     public static String mSandstormEndPosition = "";
