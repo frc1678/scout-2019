cf0a8ef5baf7730a7533758fb06b2efd345e29d2
helena <helena.p.y@icloud.com>
2019-03-16 15:51:19 -0700

    Add preload to Sandstorm screen.

--- app/src/main/java/citruscircuits/scout/utils/StormDialog.java
+++ app/src/main/java/citruscircuits/scout/utils/StormDialog.java
@@ -11,6 +11,8 @@ import citruscircuits.scout.A1A;
 import citruscircuits.scout.Managers.InputManager;
 import citruscircuits.scout.R;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.ToggleButton;
 
 public class StormDialog extends Fragment {
@@ -19,6 +21,10 @@ public class StormDialog extends Fragment {
     public static Button btn_startTimer;
     public static ToggleButton tb_hab_run;
     public static Button teleButton;
+    public static RadioGroup preload2;
+    public static RadioButton preloadCargo2;
+    public static RadioButton preloadPanel2;
+    public static RadioButton preloadNone2;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -34,8 +40,12 @@ public class StormDialog extends Fragment {
             btn_startTimer = view.findViewById(R.id.btn_timer);
             tb_hab_run = view.findViewById(R.id.tgbtn_storm_run);
             teleButton =view.findViewById(R.id.btn_to_teleop);
-
         }
+        preload2 = (RadioGroup) view.findViewById(R.id.preload2);
+        preloadCargo2 = (RadioButton) view.findViewById(R.id.preloadCargo2);
+        preloadPanel2 = (RadioButton) view.findViewById(R.id.preloadPanel2);
+        preloadNone2 = (RadioButton) view.findViewById(R.id.preloadNone2);
+
         if(A1A.cancelStormChecker){
             btn_startTimer.setPressed(true);
             Log.e("wok", "wofansegnasgk");
