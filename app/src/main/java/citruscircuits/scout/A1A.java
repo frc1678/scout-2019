2c723e38a5b3cc039ff6a776b6d533edb644175e
BenBobell <playstationruler@gmail.com>
2019-03-17 16:23:24 -0700

    Remove unecessary imports

--- app/src/main/java/citruscircuits/scout/A1A.java
+++ app/src/main/java/citruscircuits/scout/A1A.java
@@ -20,7 +20,6 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
