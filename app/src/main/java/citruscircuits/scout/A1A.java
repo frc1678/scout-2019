3b56b5eb68d7939819814adb2ebad38a2c286afc
Ben Bobell <playstationruler@gmail.com>
2019-03-30 11:04:28 -0700

    Change name in Data check activity (#85)
    * Add name spinner to DataCheck activity.
    
    * Allow Scout to change their name in DataCheck activity.
    
    * Enlarge spinner text and set name to shared preferences.
    
    * Make spinner black

--- app/src/main/java/citruscircuits/scout/A1A.java
+++ app/src/main/java/citruscircuits/scout/A1A.java
@@ -1889,22 +1889,7 @@ public class A1A extends DialogMaker implements View.OnClickListener {
 
             mRealTimeMatchData.put(compressionDic);
         }
-        try {
-            InputManager.mOneTimeMatchData.put("startingLevel", InputManager.mHabStartingPositionLevel);
-            InputManager.mOneTimeMatchData.put("crossedHabLine", InputManager.mCrossedHabLine);
-            InputManager.mOneTimeMatchData.put("startingLocation", InputManager.mHabStartingPositionOrientation);
-            InputManager.mOneTimeMatchData.put("preload", InputManager.mPreload);
-            InputManager.mOneTimeMatchData.put("isNoShow", InputManager.isNoShow);
-            InputManager.mOneTimeMatchData.put("timerStarted", InputManager.mTimerStarted);
-            InputManager.mOneTimeMatchData.put("currentCycle", InputManager.mCycleNum);
-            InputManager.mOneTimeMatchData.put("scoutName", InputManager.mScoutName);
-            InputManager.mOneTimeMatchData.put("scoutID", InputManager.mScoutId);
-            InputManager.mOneTimeMatchData.put("appVersion", InputManager.mAppVersion);
-            InputManager.mOneTimeMatchData.put("assignmentMode", InputManager.mAssignmentMode);
-            InputManager.mOneTimeMatchData.put("assignmentFileTimestamp", InputManager.mAssignmentFileTimestamp);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+
         open(A2A.class, null, false, true);
     }
 
