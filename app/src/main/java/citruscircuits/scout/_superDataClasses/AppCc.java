6ababf3eb1c0f5c7a78eac0b5e18e602df5ea9bc
Ben Bobell <playstationruler@gmail.com>
2019-06-06 21:42:06 -0700

    Comment AppCc class (#122)

--- app/src/main/java/citruscircuits/scout/_superDataClasses/AppCc.java
+++ app/src/main/java/citruscircuits/scout/_superDataClasses/AppCc.java
@@ -18,27 +18,33 @@ public class AppCc extends Application{
         Log.e("INSTANCE REF", INSTANCE.toString());
     }
 
+    //Gets any value saved into shared preferences.
     public final static SharedPreferences getSp(){
         Log.e("INSTANCE REF", INSTANCE.toString());
         return INSTANCE.getSharedPreferences(Cst.SHARED_PREF, Activity.MODE_PRIVATE);
     }
 
+    //Gets the shared preference for string values
     public static String getSp(String key, String defaultValue){
         return AppCc.getSp().getString(key, defaultValue);
     }
 
+    //Gets the shared preference for integer values
     public static int getSp(String key, int defaultValue){
         return AppCc.getSp().getInt(key, defaultValue);
     }
 
+    //Gets the shared preference for JSON values
     public static JSONObject getJSONSp(String key){
         return AppUtils.toJSONObject(AppCc.getSp(key, ""));
     }
 
+    //Sets shared preferences for string values
     public static void setSp(String key, String value) {
         AppCc.getSp().edit().putString(key, value).apply();
     }
 
+    //Sets shared preferences for integer values
     public static void setSp(String key, int value) {
         AppCc.getSp().edit().putInt(key, value).apply();
     }
